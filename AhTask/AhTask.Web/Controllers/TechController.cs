1b12d9dcf2349be8215f7fdf576db52e71e9e532
Justimiano <jalves@athenahealth.com>
2015-07-10 11:20:40 -0400

    Tech page
    I have set up the Tech page's architecture.

---
+++ AhTask/AhTask.Web/Controllers/TechController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AhTask.Web.Controllers
+{
+    public class TechController : Controller
+    {
+        // GET: Tech
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TeamFoundationServer()
+        {
+            return View();
+        }
+
+        public ActionResult TeamDevelopmentSitecore()
+        {
+            return View();
+        }
+
+        
+        public ActionResult OctopusDeploy()
+        {
+            return View();
+        }
+
+
+        public ActionResult WindowsAzure()
+        {
+            return View();
+        }
+
+        public ActionResult Sitecore()
+        {
+            return View();
+        }
+
+        public ActionResult GlassMapper()
+        {
+            return View();
+        }
+
+        public ActionResult TeamCity()
+        {
+            return View();
+        }
+
+    }
+}
