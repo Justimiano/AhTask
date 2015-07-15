56abd5fc3b98f9be312adf032ef21a306475c2f4
Justimiano <jalves@athenahealth.com>
2015-07-15 11:04:27 -0400

    Sitecore training web page
    I created the sitecore training web page.

---
+++ AhTask/AhTask.Web/Areas/Sitecore/Controllers/RealController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AhTask.Web.Areas.Sitecore.Controllers
+{
+    public class RealController : Controller
+    {
+        // GET: Sitecore/Real
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AhTask/AhTask.Web/Areas/Sitecore/SitecoreAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AhTask.Web.Areas.Sitecore
+{
+    public class SitecoreAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Sitecore";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Sitecore_default",
+                "Sitecore/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
