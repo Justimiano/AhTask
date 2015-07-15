3ccf417cf732987bb3e78ed0598c2f8dc8f081d7
Justimiano <jalves@athenahealth.com>
2015-07-15 11:04:27 -0400

    Sitecore training web page
    I created the sitecore training web page.

---
+++ AhTask/AhTask.Web/Areas/Sitecore/Controllers/DeveloperController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AhTask.Web.Areas.Sitecore.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        // GET: Sitecore/Developer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SitecoreRocks()
+        {
+            return View();
+        }
+
+        public ActionResult Presentation()
+        {
+            return View();
+        }
+
+        public ActionResult Components()
+        {
+            return View();
+        }
+    }
+}
---
+++ AhTask/AhTask.Web/Areas/Sitecore/Controllers/TrySitecoreController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AhTask.Web.Areas.Sitecore.Controllers
+{
+    public class TrySitecoreController : Controller
+    {
+        // GET: Sitecore/TrySitecore
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Installation()
+        {
+            return View();
+        }
+
+        public ActionResult Interface()
+        {
+            return View();
+        }
+
+        public ActionResult Templates()
+        {
+            return View();
+        }
+
+        public ActionResult Presentation ()
+        {
+            return View();
+        }
+    }
+}
